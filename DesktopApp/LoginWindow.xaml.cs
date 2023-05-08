02a993e26207f48a141217a6a9b13c8874d2d400
majotigartua <majotigartua@gmail.com>
2023-05-08 09:52:14 -0600

    Creation of the desktop and mobile application folders

---
+++ DesktopApp/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows; 
+
+namespace ProfessorPerformanceEvaluation
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
