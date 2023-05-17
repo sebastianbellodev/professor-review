33326406c24d82f0ea3b2c946a7d6a0f327ccc8f
Pxrsona <obando_armando28@hotmail.com>
2023-05-16 21:42:36 -0600

    Update configuration.js

--- REST-API/src/utilities/email/configuration/configuration.js
+++ REST-API/src/utilities/email/configuration/configuration.js
@@ -1,2 +1,2 @@
-export const SENDGRID_API_KEY = "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";
+export const SENDGRID_API_KEY = "";
 export const FROM_EMAIL = "obando_armando28@hotmail.com";
