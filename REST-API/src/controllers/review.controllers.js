82612aeb70a804cb0b59dc86d5f2b0800391b1fb
Pxrsona <obando_armando28@hotmail.com>
2023-05-21 21:43:00 -0600

    added checkReview and logReview

---
+++ REST-API/src/controllers/review.controllers.js
@@ -0,0 +1,41 @@
+import { pool } from "../schema/connection";
+
+export const checkReview = (req) => {
+    const {
+        idSchoolPeriod,
+        idAcademicOffering,
+        registrationNumber
+    } = req.body;
+    return Promise.resolve(
+        pool.query(
+            "Select * from review WHERE review.idSchoolPeriod = ? and review.idAcademicOffering = ? and review.registrationNumber = ?",
+            [
+                idSchoolPeriod,
+                idAcademicOffering,
+                registrationNumber
+            ]
+        )
+    );
+};
+
+export const logReview = (req) => {
+    const {
+        stars,
+        comment,
+        idSchoolPeriod,
+        idAcademicOffering,
+        registrationNumber
+    } = req.body;
+    return Promise.resolve(
+        pool.query(
+            "INSERT INTO review (`stars`, `comment`, `idSchoolPeriod`, `idAcademicOffering`, `registrationNumber`) VALUES ('?', '?', '?', '?', '?')",
+            [
+                stars,
+                comment,
+                idSchoolPeriod,
+                idAcademicOffering,
+                registrationNumber
+            ]
+        )
+    );
+};
