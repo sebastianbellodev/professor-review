0f84fc5d8d1b638ec91f120d47f90f22a86ecac5
Pxrsona <obando_armando28@hotmail.com>
2023-05-21 21:48:30 -0600

    added route post/review

---
+++ REST-API/src/routes/review.routes.js
@@ -0,0 +1,26 @@
+import Router from "express-promise-router";
+import {
+    validateToken,
+    verifyToken,
+  } from "../utilities/authentication/bearer.js";
+import { message, RES_CODE, RES_MESSAGE } from "../utilities/json/message.js";
+import { checkReview, logReview} from "../controllers/review.controllers.js";
+
+const router = Router();
+
+router.post("/review",validateToken,(req,res) => {
+    try{
+        verifyToken(req,res, async () => {
+            const [row] = await checkReview(req);
+            row.length>0
+                ? await logReview(req)
+                : message(res, RES_CODE.DATA_ALREDY,RES_MESSAGE.DATA_ALREDY_LOG)
+        });
+    }catch(err){
+        message(
+            res,RES_CODE.INTERNAL_SERVER_ERROR,
+            RES_MESSAGE.INTERAL_SERVER_ERROR,
+            err
+        );
+    }
+});
