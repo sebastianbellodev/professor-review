63403e15ac60fc2a37b627528bb54bf91d85c4b3
Pxrsona <obando_armando28@hotmail.com>
2023-05-21 21:42:12 -0600

    Added getEducationalExperienceOfFaculty

---
+++ REST-API/src/controllers/educationalExperience.controllers.js
@@ -0,0 +1,13 @@
+import { pool } from "../schema/connection";
+
+export const getEducationalExperienceOfFaculty = (req) => {
+    const {idFaculty} = req.body;
+    return Promise.resolve(
+        pool.query("select * from educationalexperience JOIN syllabus ON "+
+        "educationalexperience.idEducationalExperience = syllabus.idEducationalExperience "+
+        "JOIN educationalprogram ON syllabus.idEducationalProgram = educationalprogram.idEducationalProgram "+
+        "JOIN faculty ON faculty.idFaculty = educationalprogram.idFaculty WHERE faculty.idFaculty = ?",
+        [idFaculty]
+        )
+    );
+};
