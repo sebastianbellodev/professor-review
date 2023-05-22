9cc654efc60c4476d2eee74008d8e6d32ff11d08
Pxrsona <obando_armando28@hotmail.com>
2023-05-21 22:16:45 -0600

    added route patch review

--- REST-API/src/routes/review.routes.js
+++ REST-API/src/routes/review.routes.js
@@ -4,7 +4,7 @@ import {
     verifyToken,
   } from "../utilities/authentication/bearer.js";
 import { message, RES_CODE, RES_MESSAGE } from "../utilities/json/message.js";
-import { checkReview, logReview} from "../controllers/review.controllers.js";
+import { checkReview, logReview, updateReview} from "../controllers/review.controllers.js";
 
 const router = Router();
 
@@ -23,4 +23,22 @@ router.post("/review",validateToken,(req,res) => {
             err
         );
     }
+});
+
+router.patch("/review",validateToken,(req,res) => {
+    try{
+        verifyToken(req,res, async () => {
+            const [row] = await checkReview(req);
+            row.length>0
+                ? await updateReview(req)
+                : message(res, RES_CODE.DATA_ALREDY,RES_MESSAGE.DATA_ALREDY_LOG)
+        });
+    }catch(err){
+        message(
+            res,RES_CODE.INTERNAL_SERVER_ERROR,
+            RES_MESSAGE.INTERAL_SERVER_ERROR,
+            err
+        );
+    }
+
 });
