4f7a38c0b5da93e0a0912d4be16ed7bdf5971889
Pxrsona <obando_armando28@hotmail.com>
2023-05-21 21:47:18 -0600

    added getAllSchoolPeriods

---
+++ REST-API/src/controllers/schoolPeriods.controllers.js
@@ -0,0 +1,8 @@
+import {pool} from "../schema/connection.js";
+
+export const getAllSchoolPeriods = () => {
+    return Promise.resolve(
+            pool.query("SELECT * FROM schoolperiod")
+        );
+};
+
