22b94acf2815bb9a4a60ca1792eb52f892c7707c
Pxrsona <obando_armando28@hotmail.com>
2023-05-23 13:50:06 -0600

    added method getReportOfProfessor

---
+++ REST-API/src/controllers/report.controller.js
@@ -0,0 +1,15 @@
+import { pool } from "../schema/connection.js";
+
+export const getReportOfProfessor = (req) => {
+    const {idProfessor} = req.body;
+    return Promise.resolve(
+        pool.query("SELECT educationalexperience.name AS nombre_materia, "+
+        "COUNT(*) AS cantidad_resenas FROM review JOIN academicoffering ON "+
+        "review.idAcademicOffering = academicoffering.idAcademicOffering "+
+        "JOIN syllabus ON academicoffering.idSyllabus = syllabus.idSyllabus "+
+        "JOIN educationalexperience ON syllabus.idEducationalExperience = educationalexperience.idEducationalExperience "+
+        "WHERE academicoffering.idProfessor = ? GROUP BY educationalexperience.name",
+        [idProfessor]
+        )
+    );
+};
