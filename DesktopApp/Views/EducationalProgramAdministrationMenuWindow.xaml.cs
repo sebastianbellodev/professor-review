b03666cb0ad9660d44b80870cad4679db53b1fea
majotigartua <majotigartua@gmail.com>
2023-06-12 09:25:58 -0600

    Creation menu windows in the desktop application

---
+++ DesktopApp/Views/EducationalProgramAdministrationMenuWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace ProfessorPerformanceEvaluation.Views
+{
+    public partial class EducationalProgramManagementMenuWindow : Window
+    {
+        public EducationalProgramManagementMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogEducationalExperienceButtonClick(object sender, RoutedEventArgs e)
+        {
+            var logEducationalExperienceWindow = new LogEducationalExperienceWindow();
+            logEducationalExperienceWindow.ShowDialog();
+        }
+
+        private void ModifyEducationalExperienceButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LogProfessorButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ModifyProfessorButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void AssignEducationalExperienceToProfessorButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LogEducationalProgramButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ModifyEducationalProgramButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ManageFacultiesButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ReturnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var mainMenuWindow = new MainMenuWindow();
+            Close();
+            mainMenuWindow.Show();
+        }
+    }
+}
---
+++ DesktopApp/Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace ProfessorPerformanceEvaluation.Views
+{
+    public partial class MainMenuWindow : Window
+    {
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProfessorPerformanceEvaluationsManagementButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void EducationalProgramManagementButtonClick(object sender, RoutedEventArgs e)
+        {
+            var educationalProgramManagementMenuWindow = new EducationalProgramManagementMenuWindow();
+            Close();
+            educationalProgramManagementMenuWindow.Show();
+        }
+
+        private void StudentManagementButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SpecificReportGenerationButtonClick(object sender, RoutedEventArgs e)
+        {
+            var specificReportGenerationMenuWindow = new SpecificReportGenerationMenuWindow();
+            Close();
+            specificReportGenerationMenuWindow.Show();
+        }
+
+        private void LogOutButtonClick(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new LoginWindow();
+            Close();
+            loginWindow.Show();
+        }
+    }
+}
