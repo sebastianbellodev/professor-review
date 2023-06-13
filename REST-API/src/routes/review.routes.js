7102bebf2368575fa2e64db3fecde8ebd4270655
majotigartua <majotigartua@gmail.com>
2023-06-13 15:34:24 -0600

    Update if statements

--- REST-API/src/routes/review.routes.js
+++ REST-API/src/routes/review.routes.js
@@ -20,9 +20,11 @@ const router = Router();
 router.delete("/reviews", validateToken, async (request, response) => {
   try {
     const [row] = await deleteReview(request);
-    row.affectedRows > 0
-      ? message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_DELETE)
-      : message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.REVIEW_NOT_FOUND);
+    if (row.affectedRows > 0) {
+      message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_DELETE)
+    } else {
+      message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.REVIEW_NOT_FOUND);
+    }
   } catch (exception) {
     message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
   }
@@ -31,12 +33,12 @@ router.delete("/reviews", validateToken, async (request, response) => {
 router.patch("/reviews", validateToken, async (request, response) => {
   try {
     const [row] = await getReview(request);
-    row.length > 0
-      ? message(response, RESPONSE_CODE.BAD_REQUEST, RESPONSE_MESSAGE.REVIEW_ALREADY_REGISTERED)
-      : async () => {
-        await patchReview(request);
-        message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_PUT);
-      };
+    if (row.length > 0) {
+      message(response, RESPONSE_CODE.BAD_REQUEST, RESPONSE_MESSAGE.REVIEW_ALREADY_REGISTERED);
+    } else {
+      await patchReview(request);
+      message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_PUT);
+    }
   } catch (exception) {
     message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
   }
@@ -45,12 +47,12 @@ router.patch("/reviews", validateToken, async (request, response) => {
 router.post("/reviews", validateToken, async (request, response) => {
   try {
     const [row] = await getReview(request);
-    row.length > 0
-      ? message(response, RESPONSE_CODE.BAD_REQUEST, RESPONSE_MESSAGE.REVIEW_ALREADY_REGISTERED)
-      : async () => {
-        await postReview(request);
-        message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_POST);
-      };
+    if (row.length > 0) {
+      message(response, RESPONSE_CODE.BAD_REQUEST, RESPONSE_MESSAGE.REVIEW_ALREADY_REGISTERED)
+    } else {
+      await postReview(request);
+      message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.REVIEW_POST)
+    }
   } catch (exception) {
     message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
   }
--- REST-API/src/routes/schoolperiod.routes.js
+++ REST-API/src/routes/schoolperiod.routes.js
@@ -27,12 +27,12 @@ router.get("/schoolperiods", validateToken, async (request, response) => {
 router.post("/schoolperiods/id", validateToken, async (request, response) => {
   try {
     const [row] = await getSchoolPeriodById(request);
-    row.length > 0
-      ? () => {
-        const schoolPeriod = { schoolPeriod: row };
-        message(response, RESPONSE_CODE.OK, null, schoolPeriod);
-      }
-      : message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.SCHOOL_PERIOD_NOT_FOUND);
+    if (row.length > 0) {
+      const schoolPeriod = { schoolPeriod: row };
+      message(response, RESPONSE_CODE.OK, null, schoolPeriod);
+    } else {
+      message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.SCHOOL_PERIOD_NOT_FOUND);
+    }
   } catch (exception) {
     message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
   }
