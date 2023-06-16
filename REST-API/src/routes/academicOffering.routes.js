4958b62356ba616c1a31a4407083cbff9eb157be
Pxrsona <obando_armando28@hotmail.com>
2023-06-16 04:44:15 -0600

    yeah

--- REST-API/src/routes/academicOffering.routes.js
+++ REST-API/src/routes/academicOffering.routes.js
@@ -1,8 +1,5 @@
 import Router from "express-promise-router";
 import {
-    getAcademicOfferingById
-} from "../controllers/academicOffering.controllers.js";
-import {
     validateToken
 } from "../utilities/authentication/bearer/bearer.js";
 import {
@@ -10,20 +7,29 @@ import {
     RESPONSE_CODE,
     RESPONSE_MESSAGE
 } from "../tools/message.js";
+import {
+    getAcademicOfferingById,
+    postAcademicOffering,
+    getAcademicOffering
+} from "../controllers/academicOffering.controllers.js";
 
 const router = Router();
 
-router.post("/academicofferings/id", validateToken, async (request, response) => {
+router.post("/academicofferings", validateToken, async (request, response) => {
     try {
-        const [row] = await getAcademicOfferingById(request);
+        const [row] = await getAcademicOffering(request);
         if (row.length > 0) {
-            message(response, RESPONSE_CODE.OK, null, { academicOfferings: row });
+            message(response, RESPONSE_CODE.BAD_REQUEST, RESPONSE_MESSAGE.ACADEMIC_OFFERING_ALREADY_REGISTERED)
         } else {
-            message(response, RESPONSE_CODE.NOT_FOUND, RESPONSE_MESSAGE.EDUCATIONAL_EXPERIENCE_NOT_FOUND);
+            await postAcademicOffering(request);
+            message(response, RESPONSE_CODE.OK, RESPONSE_MESSAGE.ACADEMIC_OFFERING_POST);
         }
     } catch (exception) {
         message(response, RESPONSE_CODE.INTERNAL_SERVER_ERROR, RESPONSE_MESSAGE.INTERNAL_SERVER_ERROR);
     }
 });
+
+
+
 
 export default router;
