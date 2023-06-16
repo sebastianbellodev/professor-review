a8aefe068cf8668881e9071d95cda487f5efe5d8
Jonatan Alarcón <jonatan7890@hotmail.com>
2023-06-16 00:16:10 -0600

    Query Student Profile Binding

--- MobileApp/app/src/main/java/com/example/professorperformanceevaluation/activity/QueryStudentProfileActivity.java
+++ MobileApp/app/src/main/java/com/example/professorperformanceevaluation/activity/QueryStudentProfileActivity.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.professorperformanceevaluation.R;
 import com.example.professorperformanceevaluation.databinding.QueryStudentBinding;
+import com.example.professorperformanceevaluation.model.Student;
 import com.example.professorperformanceevaluation.viewmodel.QueryStudentProfileViewModel;
 
 public class QueryStudentProfileActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class QueryStudentProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         QueryStudentBinding binding = DataBindingUtil.setContentView(this, R.layout.query_student);
         QueryStudentProfileViewModel  viewModel = new ViewModelProvider(this).get(QueryStudentProfileViewModel.class);
+        Student student = (Student) getIntent().getSerializableExtra("student");
+        viewModel.setStudent(student);
         binding.setLifecycleOwner(this);
         binding.setQueryStudentProfileViewModel(viewModel);
     }
