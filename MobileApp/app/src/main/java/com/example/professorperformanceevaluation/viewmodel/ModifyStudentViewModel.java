48eaa6dfd15b159659b6c51fd4fe5091f2f27452
sebastian <sebtrejo@outlook.es>
2023-06-16 05:24:05 -0600

    ModifyStudentActivity added

---
+++ MobileApp/app/src/main/java/com/example/professorperformanceevaluation/viewmodel/ModifyStudentViewModel.java
@@ -0,0 +1,21 @@
+package com.example.professorperformanceevaluation.viewmodel;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.professorperformanceevaluation.R;
+import com.example.professorperformanceevaluation.activity.LogStudentActivity;
+import com.example.professorperformanceevaluation.activity.LogStudentEmailAddressPhoneNumberActivity;
+import com.example.professorperformanceevaluation.model.Student;
+
+public class ModifyStudentViewModel {
+
+
+    public void onAcceptButtonClicked() {
+
+    }
+
+    public void onCancelButtonClicked() {
+
+    }
+}
