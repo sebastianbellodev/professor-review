2e1eaa97ab17afc530c13282f182cd844007dc78
Pxrsona <obando_armando28@hotmail.com>
2023-06-19 03:00:15 -0600

    get review of studnet service added

--- MobileApp/app/src/main/java/com/example/professorperformanceevaluation/api/ReviewServiceApi.java
+++ MobileApp/app/src/main/java/com/example/professorperformanceevaluation/api/ReviewServiceApi.java
@@ -3,6 +3,7 @@ package com.example.professorperformanceevaluation.api;
 import com.example.professorperformanceevaluation.model.EducationalExperience;
 import com.example.professorperformanceevaluation.model.Response;
 import com.example.professorperformanceevaluation.model.Review;
+import com.example.professorperformanceevaluation.model.Student;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -19,7 +20,10 @@ public interface ReviewServiceApi {
     @POST("reviews/educationalexperience")
     Call<Response> getReviewsByEducationalExperience(@Header("Authorization") String token, @Body EducationalExperience educationalExperience);
 
-    @PATCH("reviews")
+    @POST("reviews/student")
+    Call<Response> getReviewsOfStudent(@Header("Authorization") String token, @Body Student student);
+
+    @PATCH("reviews/update")
     Call<Response> patch(@Header("Authorization") String token, @Body Review review);
 
     @POST("reviews")
