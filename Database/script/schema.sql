b78796893c9d062280022c4cab1ad0af30c065e6
sebastian <sebtrejo@outlook.es>
2023-06-19 10:48:11 -0600

    Project review changes

--- Database/script/schema.sql
+++ Database/script/schema.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `professorperformanceevaluation` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `professorperformanceevaluation`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: professorperformanceevaluation
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,7 +33,7 @@ CREATE TABLE `academicoffering` (
   KEY `FK_AcademicOffering_Professor_idx` (`idProfessor`),
   CONSTRAINT `FK_AcademicOffering_Professor` FOREIGN KEY (`idProfessor`) REFERENCES `professor` (`idProfessor`),
   CONSTRAINT `FK_AcademicOffering_Syllabus` FOREIGN KEY (`idSyllabus`) REFERENCES `syllabus` (`idSyllabus`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +42,7 @@ CREATE TABLE `academicoffering` (
 
 LOCK TABLES `academicoffering` WRITE;
 /*!40000 ALTER TABLE `academicoffering` DISABLE KEYS */;
+INSERT INTO `academicoffering` VALUES (1,1,13);
 /*!40000 ALTER TABLE `academicoffering` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +62,7 @@ CREATE TABLE `directory` (
   KEY `FK_Directory_Professor_idx` (`idProfessor`),
   CONSTRAINT `FK_Directory_Faculty` FOREIGN KEY (`idFaculty`) REFERENCES `faculty` (`idFaculty`),
   CONSTRAINT `FK_Directory_Professor` FOREIGN KEY (`idProfessor`) REFERENCES `professor` (`idProfessor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +71,7 @@ CREATE TABLE `directory` (
 
 LOCK TABLES `directory` WRITE;
 /*!40000 ALTER TABLE `directory` DISABLE KEYS */;
+INSERT INTO `directory` VALUES (6,15,13),(7,15,14),(8,15,15);
 /*!40000 ALTER TABLE `directory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +86,7 @@ CREATE TABLE `educationalexperience` (
   `idEducationalExperience` int NOT NULL AUTO_INCREMENT,
   `name` varchar(150) NOT NULL,
   PRIMARY KEY (`idEducationalExperience`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +95,7 @@ CREATE TABLE `educationalexperience` (
 
 LOCK TABLES `educationalexperience` WRITE;
 /*!40000 ALTER TABLE `educationalexperience` DISABLE KEYS */;
+INSERT INTO `educationalexperience` VALUES (1,'Maestría en Auditoria');
 /*!40000 ALTER TABLE `educationalexperience` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,7 +113,7 @@ CREATE TABLE `educationalprogram` (
   PRIMARY KEY (`idEducationalProgram`),
   KEY `FK_EducationalProgram_idx` (`idFaculty`),
   CONSTRAINT `FK_EducationalProgram_Faculty` FOREIGN KEY (`idFaculty`) REFERENCES `faculty` (`idFaculty`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +122,7 @@ CREATE TABLE `educationalprogram` (
 
 LOCK TABLES `educationalprogram` WRITE;
 /*!40000 ALTER TABLE `educationalprogram` DISABLE KEYS */;
-INSERT INTO `educationalprogram` VALUES (1,'Licenciatura en Administración',13),(2,'Licenciatura en Contaduría',13),(3,'Licenciatura en Gestión y Dirección de Negocios',13),(4,'Licenciatura en Sistemas Computacionales Administrativos',13),(5,'Especialización en Administración del Comercio Exterior',13),(6,'Maestría en Auditoria',13),(7,'Maestría en Gestión de las Tecnologías de Información en las Organizaciones',13),(8,'Doctorado en Ciencias Administrativas y Gestión para el Desarrollo',13),(9,'Licenciatura en Economía',14),(10,'Licenciatura en Geografía',14),(11,'Maestría en Economía Ambiental y Ecológica',14),(12,'Doctorado en Finanzas Públicas',14),(13,'Licenciatura en Estadística',15),(14,'Licenciatura en Ingeniería en Sistemas y Tecnologías de la Información',15),(15,'Licenciatura en Ingeniería de Software',15),(16,'Licenciatura en Ingeniería de Ciberseguridad e Infraestructura de Cómputo',15),(17,'Licenciatura en Redes y Servicios de Cómputo',15),(18,'Licenciatura en Tecnologías Computacionales',15),(19,'Especialización en Métodos Estadísticos',15),(20,'Maestría en Sistemas Interactivos Centrados en el Usuario',15),(21,'Maestría en Gestión de Calidad',15),(22,'Doctorado en Ciencias de la Computación',15),(23,'Licenciatura en Administración de Negocios Internacionales ',16),(24,'Licenciatura en Ciencias Políticas y Gestión Pública',16),(25,'Licenciatura en Desarrollo del Talento Humano en las Organizaciones',16),(26,'Licenciatura en Publicidad y Relaciones Públicas',16),(27,'Maestría en Gestión de Recursos Humanos, Trabajo y Organizaciones',16);
+INSERT INTO `educationalprogram` VALUES (1,'Licenciatura en Administración',13),(2,'Licenciatura en Contaduría',13),(3,'Licenciatura en Gestión y Dirección de Negocios',13),(4,'Licenciatura en Sistemas Computacionales Administrativos',13),(5,'Especialización en Administración del Comercio Exterior',13),(6,'Maestría en Auditoria',13),(7,'Maestría en Gestión de las Tecnologías de Información en las Organizaciones',13),(8,'Doctorado en Ciencias Administrativas y Gestión para el Desarrollo',13),(9,'Licenciatura en Economía',14),(10,'Licenciatura en Geografía',14),(11,'Maestría en Economía Ambiental y Ecológica',14),(12,'Doctorado en Finanzas Públicas',14),(13,'Licenciatura en Estadística',15),(14,'Licenciatura en Ingeniería en Sistemas y Tecnologías de la Información',15),(15,'Licenciatura en Ingeniería de Software',15),(16,'Licenciatura en Ingeniería de Ciberseguridad e Infraestructura de Cómputo',15),(17,'Licenciatura en Redes y Servicios de Cómputo',15),(18,'Licenciatura en Tecnologías Computacionales',15),(19,'Especialización en Métodos Estadísticos',15),(20,'Maestría en Sistemas Interactivos Centrados en el Usuario',15),(21,'Maestría en Gestión de Calidad',15),(22,'Doctorado en Ciencias de la Computación',15),(23,'Licenciatura en Administración de Negocios Internacionales ',16),(24,'Licenciatura en Ciencias Políticas y Gestión Pública',16),(25,'Licenciatura en Desarrollo del Talento Humano en las Organizaciones',16),(26,'Licenciatura en Publicidad y Relaciones Públicas',16),(27,'Maestría en Gestión de Recursos Humanos, Trabajo y Organizaciones',16),(28,'Facultad de Danza Contemporánea',2),(29,'Licenciatura en Derecho',17);
 /*!40000 ALTER TABLE `educationalprogram` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,7 +146,7 @@ CREATE TABLE `faculty` (
 
 LOCK TABLES `faculty` WRITE;
 /*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
-INSERT INTO `faculty` VALUES (1,'Facultad de Artes Plásticas'),(2,'Facultad de Danza'),(3,'Facultad de Música'),(4,'Facultad de Teatro'),(5,'Facultad de Biología'),(6,'Facultad de Ciencias Agrícolas'),(7,'Facultad de Bioanálisis'),(8,'Facultad de Enfermería'),(9,'Facultad de Medicina'),(10,'Facultad de Nutrición'),(11,'Facultad de Odontología'),(12,'Facultad de Psicología'),(13,'Facultad de Contaduría y Administración'),(14,'Facultad de Economía'),(15,'Facultad de Estadística e Informática'),(16,'Facultad de Ciencias Administrativas y Sociales'),(17,'Facultad de Derecho'),(18,'Facultad de Sociología'),(19,'Facultad de Antropología'),(20,'Facultad de Filosofía'),(21,'Facultad de Historia'),(22,'Facultad de Idiomas'),(23,'Facultad de Letras Españolas'),(24,'Facultad de Pedagogía'),(25,'Facultad de Arquitectura'),(26,'Facultad de Física'),(27,'Facultad de Matemáticas'),(28,'Facultad de Ingeniería Civil'),(29,'Facultad de Química Farmacéutica Biológica'),(30,'Facultad de Ciencias Químicas'),(31,'Facultad de Ingeniería Mécanica y Eléctrica'),(32,'Facultad de Instrumentación Electrónica');
+INSERT INTO `faculty` VALUES (1,'Facultad de Artes Plásticas'),(2,'Facultad de Danza'),(3,'Facultad de Música'),(4,'Facultad de Teatro'),(5,'Facultad de Biología'),(6,'Facultad de Ciencias Agrícolas'),(7,'Facultad de Bioanálisis'),(8,'Facultad de Enfermería'),(9,'Facultad de Medicina'),(10,'Facultad de Nutrición'),(11,'Facultad de Odontología'),(12,'Facultad de Psicología'),(13,'Facultad de Contaduría y Administración'),(14,'Facultad de Economía'),(15,'Facultad de Estadística e Informática'),(16,'Facultad de Ciencias Administrativas y Sociales'),(17,'Facultad de Derecho'),(18,'Facultad de Sociología'),(19,'Facultad de Antropología'),(20,'Facultad de Filosofía'),(21,'Facultad de Historia'),(22,'Facultad de Idiomas'),(23,'Facultad de Letras Españolas'),(24,'Facultad de Pedagogía'),(25,'Facultad de Arquitectura'),(26,'Facultad de Física'),(27,'Facultad de Matemáticas'),(28,'Facultad de Ingeniería Civil'),(29,'Facultad de Química Farmacéutica Biológica'),(30,'Facultad de Ciencias Químicas'),(32,'Facultad de Instrumentación Electrónica');
 /*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,7 +162,7 @@ CREATE TABLE `professor` (
   `name` varchar(150) NOT NULL,
   `lastName` varchar(150) NOT NULL,
   PRIMARY KEY (`idProfessor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -166,6 +171,7 @@ CREATE TABLE `professor` (
 
 LOCK TABLES `professor` WRITE;
 /*!40000 ALTER TABLE `professor` DISABLE KEYS */;
+INSERT INTO `professor` VALUES (13,'Maribel','Trejo'),(14,'Mario Alberto','Hernández'),(15,'Karim','León'),(16,'Susana','Domínguez'),(17,'Violeta','Ugalde'),(18,'José Luis','Ortíz'),(19,'María de los Ángeles','Salazar'),(20,'Lucas','Fernández'),(21,'Ximena','Fuentes'),(22,'Lourdes','Trejo');
 /*!40000 ALTER TABLE `professor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,7 +196,7 @@ CREATE TABLE `review` (
   CONSTRAINT `FK_Review_AcademicOffering` FOREIGN KEY (`idAcademicOffering`) REFERENCES `academicoffering` (`idAcademicOffering`),
   CONSTRAINT `FK_Review_SchoolPeriod` FOREIGN KEY (`idSchoolPeriod`) REFERENCES `schoolperiod` (`idSchoolPeriod`),
   CONSTRAINT `FK_Review_Student` FOREIGN KEY (`registrationNumber`) REFERENCES `student` (`registrationNumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -199,6 +205,7 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
+INSERT INTO `review` VALUES (1,5,'Buena clase',1,1,'zs19014012');
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -214,7 +221,7 @@ CREATE TABLE `schoolperiod` (
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   PRIMARY KEY (`idSchoolPeriod`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -223,6 +230,7 @@ CREATE TABLE `schoolperiod` (
 
 LOCK TABLES `schoolperiod` WRITE;
 /*!40000 ALTER TABLE `schoolperiod` DISABLE KEYS */;
+INSERT INTO `schoolperiod` VALUES (1,'2023-06-16','2023-09-26');
 /*!40000 ALTER TABLE `schoolperiod` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,7 +248,7 @@ CREATE TABLE `student` (
   `emailAddress` varchar(150) NOT NULL,
   `phoneNumber` varchar(25) DEFAULT NULL,
   `biography` text,
-  `active` bit(1) DEFAULT b'0',
+  `active` int DEFAULT '0',
   `activationDate` datetime DEFAULT NULL,
   `oneTimePassword` varchar(6) DEFAULT NULL,
   `idEducationalProgram` int NOT NULL,
@@ -256,7 +264,7 @@ CREATE TABLE `student` (
 
 LOCK TABLES `student` WRITE;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
-INSERT INTO `student` VALUES ('zs19014012','María José','Torres','zs19014012@estudiantes.uv.mx','2281590651',NULL,_binary '\0',NULL,NULL,15),('zs20015698','Armando Omar','Obando','zs20015698@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15),('zs20015699','Saraí','Castillo','zs20015699@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15),('zs20015700','Jonatan','Alarcón','zs20015700@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15),('zs20015715','Óscar Iván','Olivares','zs20015715@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15),('zs20015730','Sebastián','Bello','zs20015730@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15),('zs20015760','Álvaro','Barradas','zs20015760@estudiantes.uv.mx',NULL,NULL,_binary '\0',NULL,NULL,15);
+INSERT INTO `student` VALUES ('zs19014012','María José','Torres','zs19014012@estudiantes.uv.mx','2281590651',NULL,1,NULL,NULL,15),('zs20015698','Armando Omar','Obando','zs20015698@estudiantes.uv.mx',NULL,NULL,0,NULL,NULL,15),('zs20015699','Saraí','Castillo','zs20015699@estudiantes.uv.mx',NULL,NULL,1,NULL,NULL,15),('zs20015700','Jonatan','Alarcón','zs20015700@estudiantes.uv.mx',NULL,NULL,0,NULL,NULL,15),('zs20015715','Óscar Iván','Olivares','zs20015715@estudiantes.uv.mx',NULL,NULL,1,NULL,NULL,15),('zs20015730','Sebastián','Bello','zs20015730@estudiantes.uv.mx',NULL,NULL,0,NULL,NULL,15),('zs20015739','Martín','Hernández','zs20015732@estudiantes.uv.mx','2281604463',NULL,1,NULL,'FYBu6F',15),('zs20015760','Álvaro','Barradas','zs20015760@estudiantes.uv.mx',NULL,NULL,0,NULL,NULL,15);
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,7 +284,7 @@ CREATE TABLE `syllabus` (
   KEY `FK_Syllabus_EducationalExperience` (`idEducationalProgram`),
   CONSTRAINT `FK_Syllabus_EducationalExperience` FOREIGN KEY (`idEducationalProgram`) REFERENCES `educationalprogram` (`idEducationalProgram`),
   CONSTRAINT `FK_Syllabus_EducationalProgram` FOREIGN KEY (`idEducationalExperience`) REFERENCES `educationalexperience` (`idEducationalExperience`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -285,6 +293,7 @@ CREATE TABLE `syllabus` (
 
 LOCK TABLES `syllabus` WRITE;
 /*!40000 ALTER TABLE `syllabus` DISABLE KEYS */;
+INSERT INTO `syllabus` VALUES (1,11,1),(2,21,1);
 /*!40000 ALTER TABLE `syllabus` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -301,7 +310,7 @@ CREATE TABLE `user` (
   `registrationNumber` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`username`),
   KEY `FK_User_RegistrationNumber_idx` (`registrationNumber`),
-  CONSTRAINT `FK_User_RegistrationNumber` FOREIGN KEY (`registrationNumber`) REFERENCES `student` (`registrationNumber`)
+  CONSTRAINT `FK_User_RegistrationNumber` FOREIGN KEY (`registrationNumber`) REFERENCES `student` (`registrationNumber`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -311,7 +320,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('majotigartua','7fcca36362319dce12bdf6eb8c36695d8843aae8a282af2044974c25df628d2a','zs19014012'),('sebastianbello','566d4c109d5ab4a24c54f1c6f86bd55c3850cd8a81093bc071f6b80fc94a791c','zs20015730');
+INSERT INTO `user` VALUES ('majotigartua','b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79','zs19014012'),('sebastianbello','eef32edbe8a667280b1bd1ab77274829b77e533995c5f482b970e5b380778222','zs20015730');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -324,4 +333,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-07  8:07:35
+-- Dump completed on 2023-06-19  9:02:30
--- REST-API/src/controllers/review.controllers.js
+++ REST-API/src/controllers/review.controllers.js
@@ -3,37 +3,28 @@ import { pool } from "../schema/connection.js";
 export const deleteReview = (request) => {
   const idReview = request.body.idReview;
   return Promise.resolve(
-    pool.query(
-      "DELETE FROM\n" +
-      "review\n" +
-      "WHERE\n" +
-      "idReview = ?",
-      [idReview]
-    )
+    pool.query("DELETE FROM\n" + "review\n" + "WHERE\n" + "idReview = ?", [
+      idReview,
+    ])
   );
 };
 
 export const getReview = (request) => {
-  const { idSchoolPeriod,
-    idAcademicOffering,
-    registrationNumber } = request.body;
+  const { idSchoolPeriod, idAcademicOffering, registrationNumber } =
+    request.body;
   return Promise.resolve(
     pool.query(
       "SELECT\n" +
-      "review.*\n" +
-      "FROM\n" +
-      "review\n" +
-      "WHERE\n" +
-      "review.idSchoolPeriod = ?\n" +
-      "AND\n" +
-      "review.idAcademicOffering = ?\n" +
-      "AND\n" +
-      "review.registrationNumber = ?",
-      [
-        idSchoolPeriod,
-        idAcademicOffering,
-        registrationNumber
-      ]
+        "review.*\n" +
+        "FROM\n" +
+        "review\n" +
+        "WHERE\n" +
+        "review.idSchoolPeriod = ?\n" +
+        "AND\n" +
+        "review.idAcademicOffering = ?\n" +
+        "AND\n" +
+        "review.registrationNumber = ?",
+      [idSchoolPeriod, idAcademicOffering, registrationNumber]
     )
   );
 };
@@ -43,46 +34,44 @@ export const getReviewsOfStudent = (request) => {
   return Promise.resolve(
     pool.query(
       "SELECT\n" +
-      "review.stars, review.idReview,\n" +
-      "review.comment,\n" +
-      "CONCAT(schoolPeriod.startDate, \" - \", schoolperiod.endDate) AS schoolPeriod,\n" +
-      "educationalexperience.name AS educationalExperience,\n" +
-      "CONCAT(professor.name, \" \", professor.lastName) AS professor,\n" +
-      "CONCAT(student.name, \" \", student.lastName) AS student\n" +
-      "FROM\n" +
-      "review\n" +
-      "INNER JOIN\n" +
-      "schoolPeriod\n" +
-      "ON\n" +
-      "review.idSchoolPeriod = schoolPeriod.idSchoolPeriod\n" +
-      "INNER JOIN\n" +
-      "student\n" +
-      "ON\n" +
-      "review.registrationNumber = student.registrationNumber\n" +
-      "INNER JOIN\n" +
-      "academicOffering\n" +
-      "ON\n" +
-      "review.idAcademicOffering = academicOffering.idAcademicOffering\n" +
-      "INNER JOIN\n" +
-      "syllabus\n" +
-      "ON\n" +
-      "academicOffering.idSyllabus = syllabus.idSyllabus\n" +
-      "INNER JOIN\n" +
-      "educationalExperience\n" +
-      "ON\n" +
-      "educationalExperience.idEducationalExperience = syllabus.idEducationalExperience\n" +
-      "INNER JOIN\n" +
-      "professor\n" +
-      "ON\n" +
-      "academicOffering.idProfessor = professor.idProfessor\n" +
-      "WHERE\n" +
-      "review.registrationNumber = ?\n" +
-      "ORDER BY\n" +
-      "review.comment\n" +
-      "ASC",
-      [
-        registrationNumber
-      ]
+        "review.stars, review.idReview,\n" +
+        "review.comment,\n" +
+        'CONCAT(schoolPeriod.startDate, " - ", schoolperiod.endDate) AS schoolPeriod,\n' +
+        "educationalexperience.name AS educationalExperience,\n" +
+        'CONCAT(professor.name, " ", professor.lastName) AS professor,\n' +
+        'CONCAT(student.name, " ", student.lastName) AS student\n' +
+        "FROM\n" +
+        "review\n" +
+        "INNER JOIN\n" +
+        "schoolPeriod\n" +
+        "ON\n" +
+        "review.idSchoolPeriod = schoolPeriod.idSchoolPeriod\n" +
+        "INNER JOIN\n" +
+        "student\n" +
+        "ON\n" +
+        "review.registrationNumber = student.registrationNumber\n" +
+        "INNER JOIN\n" +
+        "academicOffering\n" +
+        "ON\n" +
+        "review.idAcademicOffering = academicOffering.idAcademicOffering\n" +
+        "INNER JOIN\n" +
+        "syllabus\n" +
+        "ON\n" +
+        "academicOffering.idSyllabus = syllabus.idSyllabus\n" +
+        "INNER JOIN\n" +
+        "educationalExperience\n" +
+        "ON\n" +
+        "educationalExperience.idEducationalExperience = syllabus.idEducationalExperience\n" +
+        "INNER JOIN\n" +
+        "professor\n" +
+        "ON\n" +
+        "academicOffering.idProfessor = professor.idProfessor\n" +
+        "WHERE\n" +
+        "review.registrationNumber = ?\n" +
+        "ORDER BY\n" +
+        "review.comment\n" +
+        "ASC",
+      [registrationNumber]
     )
   );
 };
@@ -92,43 +81,43 @@ export const getReviewsByEducationalExperience = (request) => {
   return Promise.resolve(
     pool.query(
       "SELECT\n" +
-      "review.stars,\n" +
-      "review.comment,\n" +
-      "CONCAT(schoolPeriod.startDate, \" - \", schoolperiod.endDate) AS schoolPeriod,\n" +
-      "educationalexperience.name AS educationalExperience,\n" +
-      "CONCAT(professor.name, \" \", professor.lastName) AS professor,\n" +
-      "CONCAT(student.name, \" \", student.lastName) AS student\n" +
-      "FROM\n" +
-      "review\n" +
-      "INNER JOIN\n" +
-      "schoolPeriod\n" +
-      "ON\n" +
-      "review.idSchoolPeriod = schoolPeriod.idSchoolPeriod\n" +
-      "INNER JOIN\n" +
-      "student\n" +
-      "ON\n" +
-      "review.registrationNumber = student.registrationNumber\n" +
-      "INNER JOIN\n" +
-      "academicOffering\n" +
-      "ON\n" +
-      "review.idAcademicOffering = academicOffering.idAcademicOffering\n" +
-      "INNER JOIN\n" +
-      "syllabus\n" +
-      "ON\n" +
-      "academicOffering.idSyllabus = syllabus.idSyllabus\n" +
-      "INNER JOIN\n" +
-      "educationalExperience\n" +
-      "ON\n" +
-      "educationalExperience.idEducationalExperience = syllabus.idEducationalExperience\n" +
-      "INNER JOIN\n" +
-      "professor\n" +
-      "ON\n" +
-      "academicOffering.idProfessor = professor.idProfessor\n" +
-      "WHERE\n" +
-      "syllabus.idEducationalExperience = ?\n" +
-      "ORDER BY\n" +
-      "review.comment\n" +
-      "ASC",
+        "review.stars,\n" +
+        "review.comment,\n" +
+        'CONCAT(schoolPeriod.startDate, " - ", schoolperiod.endDate) AS schoolPeriod,\n' +
+        "educationalexperience.name AS educationalExperience,\n" +
+        'CONCAT(professor.name, " ", professor.lastName) AS professor,\n' +
+        'CONCAT(student.name, " ", student.lastName) AS student\n' +
+        "FROM\n" +
+        "review\n" +
+        "INNER JOIN\n" +
+        "schoolPeriod\n" +
+        "ON\n" +
+        "review.idSchoolPeriod = schoolPeriod.idSchoolPeriod\n" +
+        "INNER JOIN\n" +
+        "student\n" +
+        "ON\n" +
+        "review.registrationNumber = student.registrationNumber\n" +
+        "INNER JOIN\n" +
+        "academicOffering\n" +
+        "ON\n" +
+        "review.idAcademicOffering = academicOffering.idAcademicOffering\n" +
+        "INNER JOIN\n" +
+        "syllabus\n" +
+        "ON\n" +
+        "academicOffering.idSyllabus = syllabus.idSyllabus\n" +
+        "INNER JOIN\n" +
+        "educationalExperience\n" +
+        "ON\n" +
+        "educationalExperience.idEducationalExperience = syllabus.idEducationalExperience\n" +
+        "INNER JOIN\n" +
+        "professor\n" +
+        "ON\n" +
+        "academicOffering.idProfessor = professor.idProfessor\n" +
+        "WHERE\n" +
+        "syllabus.idEducationalExperience = ?\n" +
+        "ORDER BY\n" +
+        "review.comment\n" +
+        "ASC",
       [idEducationalExperience]
     )
   );
@@ -139,62 +128,74 @@ export const getReviewsByProfessor = (request) => {
   return Promise.resolve(
     pool.query(
       "SELECT\n" +
-      "review.idReview, review.stars, review.comment, review.registrationNumber\n" +
-      "FROM\n" +
-      "review\n" +
-      "INNER JOIN\n" +
-      "academicoffering\n" +
-      "ON\n" +
-      "review.idAcademicOffering = academicoffering.idAcademicOffering\n" +
-      "INNER JOIN\n" +
-      "professor\n" +
-      "ON\n" +
-      "academicoffering.idProfessor = professor.idProfessor\n" +
-      "WHERE\n" +
-      "professor.idProfessor = ?",
+        "review.stars,\n" +
+        "review.comment,\n" +
+        'CONCAT(schoolPeriod.startDate, " - ", schoolperiod.endDate) AS schoolPeriod,\n' +
+        "educationalexperience.name AS educationalExperience,\n" +
+        'CONCAT(professor.name, " ", professor.lastName) AS professor,\n' +
+        'CONCAT(student.name, " ", student.lastName) AS student\n' +
+        "FROM\n" +
+        "review\n" +
+        "INNER JOIN\n" +
+        "schoolPeriod\n" +
+        "ON\n" +
+        "review.idSchoolPeriod = schoolPeriod.idSchoolPeriod\n" +
+        "INNER JOIN\n" +
+        "student\n" +
+        "ON\n" +
+        "review.registrationNumber = student.registrationNumber\n" +
+        "INNER JOIN\n" +
+        "academicOffering\n" +
+        "ON\n" +
+        "review.idAcademicOffering = academicOffering.idAcademicOffering\n" +
+        "INNER JOIN\n" +
+        "syllabus\n" +
+        "ON\n" +
+        "academicOffering.idSyllabus = syllabus.idSyllabus\n" +
+        "INNER JOIN\n" +
+        "educationalExperience\n" +
+        "ON\n" +
+        "educationalExperience.idEducationalExperience = syllabus.idEducationalExperience\n" +
+        "INNER JOIN\n" +
+        "professor\n" +
+        "ON\n" +
+        "academicOffering.idProfessor = professor.idProfessor\n" +
+        "WHERE\n" +
+        "academicOffering.idProfessor = ?\n" +
+        "ORDER BY\n" +
+        "review.comment\n" +
+        "ASC",
       [idProfessor]
     )
   );
 };
 
 export const patchReview = (request) => {
-  const { idReview,
-    stars,
-    comment,
-    idSchoolPeriod } = request.body;
+  const { idReview, stars, comment, idSchoolPeriod } = request.body;
   return Promise.resolve(
     pool.query(
       "UPDATE `review` SET `stars` = ?, `comment` = ?, `idSchoolPeriod` = ? WHERE (`idReview` = ?);",
-      [
-        stars,
-        comment,
-        idSchoolPeriod,
-        idReview
-      ]
+      [stars, comment, idSchoolPeriod, idReview]
     )
   );
 };
 
 export const postReview = (request) => {
-  const { stars,
+  const {
+    stars,
     comment,
     idSchoolPeriod,
     idAcademicOffering,
-    registrationNumber } = request.body;
+    registrationNumber,
+  } = request.body;
   return Promise.resolve(
     pool.query(
       "INSERT INTO\n" +
-      "review\n" +
-      "(stars, comment, idSchoolPeriod, idAcademicOffering, registrationNumber)\n" +
-      "VALUES\n" +
-      "(?, ?, ?, ?, ?)",
-      [
-        stars,
-        comment,
-        idSchoolPeriod,
-        idAcademicOffering,
-        registrationNumber
-      ]
+        "review\n" +
+        "(stars, comment, idSchoolPeriod, idAcademicOffering, registrationNumber)\n" +
+        "VALUES\n" +
+        "(?, ?, ?, ?, ?)",
+      [stars, comment, idSchoolPeriod, idAcademicOffering, registrationNumber]
     )
   );
 };
